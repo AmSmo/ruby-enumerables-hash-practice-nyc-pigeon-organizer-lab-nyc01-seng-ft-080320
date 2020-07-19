283b7cbaae31a76d9109abf47fe769048470c64f
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 20:34:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,7 @@ require "pry"
 
 
 def nyc_pigeon_organizer(data)
-  pigeons = add_bird(data)
-  
-  pigeons
-end
-
-def add_bird(data)
-  pigeons = {}
-   data.each do |attribute, assignments|
+  pigeons = data.each do |attribute, assignments|
     assignments.each do |true_answer, bird_array|
       bird_array.each do |bird|
         if pigeons[bird] == nil
