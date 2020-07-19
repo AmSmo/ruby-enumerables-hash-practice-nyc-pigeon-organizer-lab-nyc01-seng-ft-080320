9b82ac4546f6a42dc885b08dd7a820c191f5196e
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 20:35:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+   data.each do |attribute, assignments|
+    assignments.each do |true_answer, bird_array|
+      bird_array.each do |bird|
+        if pigeons[bird] == nil
+          pigeons[bird] = {attribute => [true_answer.to_s]}
+        else
+          if pigeons[bird][attribute] == nil
+            pigeons[bird][attribute] = [true_answer.to_s]
+          else
+            pigeons[bird][attribute] << true_answer.to_s
+          end
+        end
+      end
+    end
+  end
+  pigeons
 end
